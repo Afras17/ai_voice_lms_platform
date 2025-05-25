e1de5aeeea0770e094c52761a932deda4c21cd09
Afras Rahim <afrasrahim11@gamil.com>
2025-05-25 17:00:35 +0530

    Companion Bilder form done

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import { recentSessions } from "@/constants";
+// import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <main>
+      <h1>Popular Companions</h1>
+
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Nuera network"
+          topic="Nuera network"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Nuera network"
+          topic="Nuera network"
+          subject="maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Nuera network"
+          topic="Nuera network"
+          subject="English"
+          duration={45}
+          color="#ffda6e"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
